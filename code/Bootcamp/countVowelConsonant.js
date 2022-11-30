05f5d3cfa5e03fc715c66dc31d3253e6df331960
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-11-29 21:42:09 -0800

    add some more

--- code/Bootcamp/countVowelConsonant.js
+++ code/Bootcamp/countVowelConsonant.js
@@ -9,7 +9,22 @@
 // solution(s) = 8.
 // The letters in s, converted to 1s and 2s and added together as described above: 1 + 2 + 2 + 2 + 1 = 8.
 
+const sum = (str) => {
+    const vowelValue = 1;
+    const consonantValue = 2;
 
+    //turn the input into a string
+    const string = str.toString();
+    //loop through the string
+    for (let i = 0; i <= string.length; i++) {
+       //if a vowel, add to vowel count
+        if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u") {
+            
+        } else {
+            
+        }
+    }
+}
 
 
 
