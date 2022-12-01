ae678ea814870959ec8a5214425028a37fba5ef5
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-11-30 16:12:09 -0800

    add some

---
+++ code/Bootcamp/array-Median.js
@@ -0,0 +1,8 @@
+// Given a sequence of integers, find its median.
+
+// Example
+
+// For sequence = [-1, 3, -2, 2], the output should be
+// solution(sequence) = 0.5;
+// For sequence = [1, 3, 2], the output should be
+// solution(sequence) = 2.
